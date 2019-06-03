580aad587d70541b0f35561461b77570821498c0
Gerson Espinal <gerson.espinal@gmail.com>
2019-06-03 10:59:59 -0400

    Fixed homepage navbar css

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,23 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div id="navContainer">
-            <nav>
-                <Link to="/">
-                <img src="https://library.kissclipart.com/20181213/oge/kissclipart-bakery-shop-icon-png-clipart-bakery-computer-icons-8fa7abc002a9ae3c.png" alt="Shop Logo" id="logo" />
-                </Link>
-                <h1>Yums Bakery Shop</h1>
-                <div className="navbar">
-                    <div>
-                        <Link to="/menu" id="link"> Menu </Link>
-                        <Link to="/aboutus" id="link"> About Us </Link>
-                    </div>
-                </div>
-            </nav>
+  return (
+    <div id="navContainer">
+      <nav>
+        <div id="logo-name">
+          <Link to="/">
+            <img
+              src="https://library.kissclipart.com/20181213/oge/kissclipart-bakery-shop-icon-png-clipart-bakery-computer-icons-8fa7abc002a9ae3c.png"
+              alt="Shop Logo"
+              id="logo"
+            />
+          </Link>
+          <h1>Yums Bakery Shop</h1>
         </div>
-    )
-}
+        <div className="nav-links">
+          <div>
+            <Link to="/menu" id="link">
+              {" "}
+              Menu{" "}
+            </Link>
+            <Link to="/aboutus" id="link">
+              {" "}
+              About Us{" "}
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
